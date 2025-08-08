40e17693324703bc22542c5a01c2ad5c69d648a3
hudha8590 <hudhathazhathayil@gmail.com>
2025-08-08 10:18:28 +0530

    message

---
+++ questions2.js
@@ -0,0 +1,26 @@
+//b.
+// const orders=[200,450,130,50,1000,20];
+// orders.forEach(function(log){
+//     console.log(log)
+// })
+
+//c.
+//const orders=[200,450,130,50,1000,20];
+// const filtered=orders.filter(function(order){
+//     return order>100
+// })
+// console.log(filtered)
+
+//d.
+// const orders=[200,450,130,50,1000,20];
+// const mapped=orders.map(function(discount){
+//     return discount-10
+// });
+// console.log(mapped)
+
+//e.
+// const orders=[200,450,130,50,1000,20];
+// const sum=arr.reduce(function(a,b){
+//     return a+b
+// })
+// console.log(sum)
